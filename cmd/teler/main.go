b62ff1f6dfa86a1acca31c68353f72e8f05cd318
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-24 17:02:01 +0700

    :wheelchair: Improves accessibility

--- cmd/teler/main.go
+++ cmd/teler/main.go
@@ -1,42 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"os"
-	"sync"
+	"runtime"
+
+	"github.com/kitabisa/teler/internal/runner"
 )
 
+func init() {
+	cpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpu + 1)
+}
+
 func main() {
-	concurrency := 20
-	jobs := make(chan string)
-	var wg sync.WaitGroup
-
-	flag.IntVar(&concurrency, "c", 20, "Set the concurrency level")
-	flag.Parse()
-
-	for i := 0; i < concurrency; i++ {
-		wg.Add(1)
-		go func() {
-			for log := range jobs {
-				fmt.Println(log)
-				// Superman flying starts here
-			}
-			wg.Done()
-		}()
-	}
-
-	sc := bufio.NewScanner(os.Stdin)
-	for sc.Scan() {
-		jobs <- sc.Text()
-	}
-
-	close(jobs)
-
-	if err := sc.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to read input: %s\n", err)
-	}
-
-	wg.Wait()
+	// Parse the command line flags
+	options := runner.ParseOptions()
+	runner.New(options)
 }
