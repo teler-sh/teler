c26a76b6a1d3e3fd8026fee18a1a211e743daad1
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-25 08:02:41 +0700

    :heavy_plus_sign: Adds updater flags

--- internal/runner/options.go
+++ internal/runner/options.go
@@ -12,6 +12,7 @@ type Options struct {
 	Concurrency int    // Set the concurrent level
 	ConfigFile  string // Specifies the config to use
 	Stdin       bool   // Stdin specifies whether stdin input was given to the process
+	Update      bool   // Updating resources to latest
 
 	Config *parsers.Config
 }
@@ -20,11 +21,18 @@ type Options struct {
 func ParseOptions() *Options {
 	options := &Options{}
 
-	flag.IntVar(&options.Concurrency, "c", 10, "Set the concurrency level")
 	flag.StringVar(&options.ConfigFile, "f", "", "teler configuration file")
+	flag.IntVar(&options.Concurrency, "c", 10, "Set the concurrency level")
+	flag.BoolVar(&options.Update, "u", false, "Update teler resources to the latest")
 	flag.Parse()
 
-	// Show the user the banner
+	// Check update flag
+	if options.Update {
+		// rsrc, _ := parsers.GetResources()
+		// TODO
+	}
+
+	// Show user the banner
 	showBanner()
 
 	// Check if stdin pipe was given
