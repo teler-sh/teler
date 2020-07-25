ed6434491f9c15408fd8e1e801fedd2d903a4961
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-25 08:07:09 +0700

    :package: Add requests package

---
+++ pkg/requests/client.go
@@ -0,0 +1,30 @@
+package requests
+
+import (
+	"crypto/tls"
+	"net"
+	"net/http"
+	"time"
+)
+
+func Client() *http.Client {
+	tr := &http.Transport{
+		MaxIdleConns:    30,
+		IdleConnTimeout: time.Second,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext: (&net.Dialer{
+			Timeout:   time.Second * 10,
+			KeepAlive: time.Second,
+		}).DialContext,
+	}
+
+	re := func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &http.Client{
+		Transport:     tr,
+		CheckRedirect: re,
+		Timeout:       time.Second * 10,
+	}
+}
