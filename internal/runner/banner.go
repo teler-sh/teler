5dd78ee5d7c1ade11e6e6944dabc5fef18b9dd0f
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-27 06:36:39 +0700

    :speech_balloon: Update banner

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -4,8 +4,8 @@ import "github.com/projectdiscovery/gologger"
 
 // showBanner is used to show the banner to the user
 func showBanner() {
-	gologger.Printf("%s\n", Banner)
-	gologger.Printf("\t%s\n\n", Email)
-	gologger.Labelf("This tool is under development.\n")
-	gologger.Labelf("Please send a report if an error occurs.\n")
+	gologger.Printf("%s\n\n", banner)
+	gologger.Printf("\t%s\n\n", email)
+	gologger.Labelf("This tool is under development!\n")
+	gologger.Labelf("Please submit a report if an error occurs.\n")
 }
