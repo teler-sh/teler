c290bd8ef8849b8ebd1101ec36da047723522316
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-27 06:38:15 +0700

    :wrench: Adds version & override help flag

--- internal/runner/options.go
+++ internal/runner/options.go
@@ -2,6 +2,9 @@ package runner
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"strings"
 
 	e "github.com/kitabisa/teler/pkg/errors"
 	"github.com/kitabisa/teler/pkg/parsers"
@@ -13,6 +16,7 @@ type Options struct {
 	ConfigFile  string // Specifies the config to use
 	Stdin       bool   // Stdin specifies whether stdin input was given to the process
 	Update      bool   // Updating resources to latest
+	Version     bool   // Updating resources to latest
 
 	Config *parsers.Config
 }
@@ -21,15 +25,45 @@ type Options struct {
 func ParseOptions() *Options {
 	options := &Options{}
 
-	flag.StringVar(&options.ConfigFile, "f", "", "teler configuration file")
-	flag.IntVar(&options.Concurrency, "c", 10, "Set the concurrency level")
-	flag.BoolVar(&options.Update, "u", false, "Update teler resources to the latest")
+	flag.StringVar(&options.ConfigFile, "f", "", "")
+	flag.StringVar(&options.ConfigFile, "file", "", "")
+
+	flag.IntVar(&options.Concurrency, "c", 20, "")
+	flag.IntVar(&options.Concurrency, "concurrent", 20, "")
+
+	flag.BoolVar(&options.Update, "u", false, "")
+	flag.BoolVar(&options.Update, "update", false, "")
+
+	flag.BoolVar(&options.Version, "v", false, "")
+	flag.BoolVar(&options.Version, "version", false, "")
+
+	// Override help flag
+	flag.Usage = func() {
+		h := []string{
+			"",
+			"Options:",
+			"  -f, --file <FILE>           teler configuration file",
+			"  -c, --concurrent <i>        Set the concurrency level to process log (default: 20)",
+			"  -u, --update                Update all teler resources to the latest",
+			"  -v, --version               Show current teler version",
+			"",
+		}
+
+		showBanner()
+		fmt.Fprintf(os.Stderr, strings.Join(h, "\n"))
+	}
+
 	flag.Parse()
 
 	// Check update flag
 	if options.Update {
 		// rsrc, _ := parsers.GetResources()
 		// TODO
+	}
+
+	// Show current version & exit
+	if options.Version {
+		showVersion()
 	}
 
 	// Show user the banner
