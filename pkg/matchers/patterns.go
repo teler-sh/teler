58eb968c3d64cec1ddc6eb24e0dbfd0b69712bb8
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-27 08:53:36 +0700

    :wrench: Add pattern for Telegram parse mode

--- pkg/matchers/patterns.go
+++ pkg/matchers/patterns.go
@@ -1,6 +1,7 @@
 package matchers
 
 const (
-	PatternToken    = `^(xox[p|b|o|a]-\d{10,12}-\d{12}-\w+)|(\d{9}:[a-zA-Z0-9_-]{35})$`
-	PatternHexcolor = `^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$`
+	PatternToken     = `^(xox[p|b|o|a]-\d{10,12}-\d{12}-\w+)|(\d{9}:[a-zA-Z0-9_-]{35})$`
+	PatternHexcolor  = `^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$`
+	PatternParseMode = `^(Markdown(?:V2)?|HTML)$`
 )
