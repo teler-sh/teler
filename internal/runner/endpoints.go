34f94987b521d6ae210d1af5d0de7b79f8316f1b
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-27 14:39:46 +0700

    :heavy_plus_sign: Add Google 204 endpoint (check connection purposes)

--- internal/runner/endpoints.go
+++ internal/runner/endpoints.go
@@ -3,4 +3,5 @@ package runner
 const (
 	SlackAPI    = "https://slack.com/api/chat.postMessage"
 	TelegramAPI = "https://api.telegram.org/bot:token/sendMessage"
+	Google204   = "http://clients3.google.com/generate_204"
 )
