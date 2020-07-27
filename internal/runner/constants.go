e6ff351fc10681b5a944a992a3e0f7ba2f8e1d5f
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-27 08:23:47 +0700

    :truck: Moves constants API endpoints to single file

--- internal/runner/constants.go
+++ internal/runner/constants.go
@@ -10,8 +10,3 @@ const (
 	\__/\__/_/\__/_/   
 	                v` + version
 )
-
-const (
-	SlackAPI    = "https://slack.com/api/chat.postMessage"
-	TelegramAPI = "https://api.telegram.org/bot:token/sendMessage"
-)
---
+++ internal/runner/endpoints.go
@@ -0,0 +1,6 @@
+package runner
+
+const (
+	SlackAPI    = "https://slack.com/api/chat.postMessage"
+	TelegramAPI = "https://api.telegram.org/bot:token/sendMessage"
+)
