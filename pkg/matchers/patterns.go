068bc3800e033489ebc9c562bb790082555257b1
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-27 09:10:17 +0700

    :wrench: Add pattern for Slack channel

--- pkg/matchers/patterns.go
+++ pkg/matchers/patterns.go
@@ -2,6 +2,7 @@ package matchers
 
 const (
 	PatternToken     = `^(xox[p|b|o|a]-\d{10,12}-\d{12}-\w+)|(\d{9}:[a-zA-Z0-9_-]{35})$`
+	PatternChannel   = `^[A-Z0-9]{9,12}$`
 	PatternHexcolor  = `^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$`
 	PatternParseMode = `^(Markdown(?:V2)?|HTML)$`
 )
