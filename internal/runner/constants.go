6d683564e15c9a544425ad34466ab5524f2e2aec
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-28 12:57:32 +0700

    :bookmark_tabs: Updates usage & examples

--- internal/runner/constants.go
+++ internal/runner/constants.go
@@ -11,7 +11,7 @@ const (
 	                v` + version
 
 	usage = `  [buffers] | teler [options]
-  teler -f [...] [options]`
-	example = `  teler -f [...] -i /var/log/nginx/access.log
-  [kubectl logs|tail -f|cat] ... | teler -f [...] -c 25`
+  teler -c [...] [options]`
+	example = `  teler -c [...] -i /var/log/nginx/access.log
+  [kubectl logs|tail -f|cat] ... | teler -c [...] -x 25`
 )
