20cf65a9ca12991be81b9e152f2630cbfe1805b9
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-28 09:41:36 +0700

    :key: Add log format pattern

--- pkg/matchers/patterns.go
+++ pkg/matchers/patterns.go
@@ -5,4 +5,5 @@ const (
 	PatternChannel   = `^[A-Z0-9]{9,12}$`
 	PatternHexcolor  = `^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$`
 	PatternParseMode = `^(Markdown(?:V2)?|HTML)$`
+	PatternLogformat = `\$(remote_(addr|user)|request_(method|uri|protocol)|http_(referer|user_agent))`
 )
