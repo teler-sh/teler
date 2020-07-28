a724784337fedc4708831e2e8428d847f081c5be
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-28 12:54:46 +0700

    :bookmark_tabs: Adds constant error messages for input log & configuration files

--- pkg/errors/messages.go
+++ pkg/errors/messages.go
@@ -4,4 +4,6 @@ const (
 	ErrCheckConfig    = "please check your config file"
 	ErrConfigValidate = "Only validates :key; " + ErrCheckConfig
 	ErrAlertProvider  = "Provider \":platform\" not available; " + ErrCheckConfig
+	ErrNoInputLog     = "No input logs provided"
+	ErrNoInputConfig  = "No config file specified"
 )
