c4fe95e86f93bf954f6857d1433d9c65ec91f171
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-28 09:10:45 +0700

    :recycle: Refactors configuration (merging filters to threat rules)

--- configs/configs.go
+++ configs/configs.go
@@ -1,12 +1,44 @@
 package configs
 
-type config []struct {
-	Category string `yaml:"cat"`
-	URL      string `yaml:"url"`
-	Content  string
+// Resources ...
+type Resources struct {
+	Threat []struct {
+		Category string
+		URL      string
+		Content  string
+	}
 }
 
-type Resources struct {
-	Threat config `yaml:"threat"`
-	Filter config `yaml:"filter"`
+// Init resources
+func Init() *Resources {
+	resource := &Resources{
+		Threat: []struct {
+			Category string
+			URL      string
+			Content  string
+		}{
+			{
+				Category: "Common Web Attack",
+				URL:      "https://raw.githubusercontent.com/enygma/expose/master/src/Expose/filter_rules.json",
+			},
+			{
+				Category: "Bad IP Address",
+				URL:      "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/_generator_lists/bad-ip-addresses.list",
+			},
+			{
+				Category: "Bad Refferer",
+				URL:      "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/_generator_lists/bad-referrers.list",
+			},
+			{
+				Category: "Bad Crawler",
+				URL:      "https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/raw/Crawlers.txt",
+			},
+			{
+				Category: "Directory Bruteforce",
+				URL:      "https://raw.githubusercontent.com/maurosoria/dirsearch/master/db/dicc.txt",
+			},
+		},
+	}
+
+	return resource
 }
