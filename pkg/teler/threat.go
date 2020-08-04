0c1a4c5e5ff35577f0155f00ef0cdbd3db7574ab
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-08-04 19:32:45 +0700

    :recycle: Refactor analyzer

--- pkg/teler/threat.go
+++ pkg/teler/threat.go
@@ -1,22 +1,23 @@
 package teler
 
 import (
+	"fmt"
 	"net/url"
 	"reflect"
+	"strings"
 	"unicode/utf8"
-	// "strings"
 
 	"github.com/kitabisa/teler/common"
 	"github.com/kitabisa/teler/configs"
 	"github.com/kitabisa/teler/pkg/matchers"
 	"github.com/projectdiscovery/gologger"
 	"github.com/satyrius/gonx"
+	"github.com/valyala/fastjson"
 )
-
-var match bool
 
 // Analyze logs from threat resources
 func Analyze(options *common.Options, logs *gonx.Entry) {
+	var match bool
 	log := make(map[string]string)
 	resource := configs.Get()
 
@@ -25,44 +26,64 @@ func Analyze(options *common.Options, logs *gonx.Entry) {
 		log[field.String()] = fields.MapIndex(field).String()
 	}
 
-	for i := 0; i < len(resource.Threat); i++ {
-		threat := reflect.ValueOf(&resource.Threat[i]).Elem()
-		cat := threat.FieldByName("Category").String()
-		con := threat.FieldByName("Content").String()
-		exc := threat.FieldByName("Exclude").Bool()
+	go func() {
+		for i := 0; i < len(resource.Threat); i++ {
+			threat := reflect.ValueOf(&resource.Threat[i]).Elem()
+			cat := threat.FieldByName("Category").String()
+			con := threat.FieldByName("Content").String()
+			exc := threat.FieldByName("Exclude").Bool()
 
-		if exc {
-			continue
-		}
-
-		switch cat {
-		case "Common Web Attack":
-		case "Bad Crawler":
-		case "Bad IP Address":
-			ip := "(?m)^" + log["remote_addr"]
-			match = matchers.IsMatch(ip, con)
-			detect(match, cat, log["remote_addr"], log["time_local"])
-		case "Bad Referrer":
-			if log["http_referer"] == "-" {
-				continue
-			}
-			req, _ := url.Parse(log["http_referer"])
-			ref := req.Path
-			if req.Host != "" {
-				ref = req.Host
-			}
-			match = matchers.IsMatch(ref, con)
-			detect(match, cat, log["http_referer"], log["time_local"])
-		case "Directory Bruteforce":
-			req, _ := url.Parse(log["request_uri"])
-			if log["status"] == "200" || req.Path == "/" {
+			if exc {
 				continue
 			}
 
-			match = matchers.IsMatch(trimFirst(req.Path), con)
-			detect(match, cat, log["request_uri"], log["time_local"])
+			switch cat {
+			case "Common Web Attack":
+				// TODO:
+				// parse, _ := fastjson.Parse(con)
+				// cwa, _ := parse.Object()
+				// cwa.Visit(func(k []byte, v *fastjson.Value) {
+				// 	fmt.Printf("%#v", v)
+				// })
+				// os.Exit(1)
+			case "Bad Crawler":
+				for _, pat := range strings.Split(con, "\n") {
+					match = matchers.IsMatch(pat, log["http_user_agent"])
+					if match {
+						break
+					}
+				}
+				detect(match, cat, log["http_user_agent"], log["time_local"])
+			case "Bad IP Address":
+				ip := "(?m)^" + log["remote_addr"]
+				match = matchers.IsMatch(ip, con)
+				detect(match, cat, log["remote_addr"], log["time_local"])
+			case "Bad Referrer":
+				ref := "(?m)^"
+				if log["http_referer"] == "-" {
+					continue
+				}
+				req, _ := url.Parse(log["http_referer"])
+				ref += req.Path
+				if req.Host != "" {
+					ref += req.Host
+				}
+				match = matchers.IsMatch(ref, con)
+				detect(match, cat, log["http_referer"], log["time_local"])
+			case "Directory Bruteforce":
+				req, _ := url.Parse(log["request_uri"])
+				if matchers.IsMatch("^(2|3)[0-9]{2}", log["status"]) {
+					continue
+				}
+
+				if req.Path != "/" {
+					match = matchers.IsMatch(trimFirst(req.Path), con)
+					detect(match, cat, log["request_uri"], log["time_local"])
+					fmt.Println(log["status"])
+				}
+			}
 		}
-	}
+	}()
 }
 
 func trimFirst(s string) string {
