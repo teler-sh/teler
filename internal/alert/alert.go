8ee2399d134e930e394c3e7b762c8fe04e2223ce
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-10-20 07:17:22 +0700

    :package: Add alerting packages

---
+++ internal/alert/alert.go
@@ -0,0 +1,32 @@
+package alert
+
+import (
+	"reflect"
+	"strings"
+
+	"ktbs.dev/teler/common"
+)
+
+// New will initialize notification provider & send threat alerts
+func New(options *common.Options, log map[string]string) {
+	config := options.Configs
+
+	if config.Alert.Active {
+		provider := strings.Title(config.Alert.Provider)
+		field := reflect.ValueOf(&config.Notifications).Elem().FieldByName(provider)
+
+		switch provider {
+		case "Slack":
+			toSlack(
+				field.FieldByName("Token").String(),
+				field.FieldByName("Channel").String(),
+				field.FieldByName("Color").String(),
+				log,
+			)
+		case "Telegram":
+			// TODO
+		case "Discord":
+			// TODO
+		}
+	}
+}
---
+++ internal/alert/slack.go
@@ -0,0 +1,65 @@
+package alert
+
+import (
+	"github.com/slack-go/slack"
+)
+
+func toSlack(token string, channel string, color string, log map[string]string) {
+	api := slack.New(token)
+	reason := slack.Attachment{
+		AuthorName: ":warning: teler Alert",
+		Title:      log["category"],
+		Color:      color,
+	}
+	request := slack.Attachment{
+		Title: "Request",
+		Text:  log[log["element"]],
+		Color: color,
+	}
+	fields := slack.Attachment{
+		Color: color,
+		Fields: []slack.AttachmentField{
+			{
+				Title: "Date",
+				Value: log["time_local"],
+				Short: true,
+			},
+			{
+				Title: "Referrer",
+				Value: log["http_referer"],
+				Short: true,
+			},
+			{
+				Title: "User-Agent",
+				Value: log["http_user_agent"],
+				Short: true,
+			},
+			{
+				Title: "Status code",
+				Value: log["status"],
+				Short: true,
+			},
+			{
+				Title: "Bytes sent",
+				Value: log["body_bytes_sent"],
+				Short: true,
+			},
+			{
+				Title: "Upstream name",
+				Value: log["proxy_upstream_name"],
+				Short: true,
+			},
+			{
+				Title: "Upstream address",
+				Value: log["upstream_addr"],
+				Short: true,
+			},
+		},
+	}
+
+	api.PostMessage(
+		channel,
+		slack.MsgOptionAttachments(reason, request, fields),
+		slack.MsgOptionAsUser(true),
+	)
+}
