4532670fb08dcfc8199fc7ac80cc7c4933fa2500
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2020-10-28 05:14:54 +0100

    Add gologger and refactor logic telegram

--- internal/alert/telegram.go
+++ internal/alert/telegram.go
@@ -8,26 +8,29 @@ import (
 	"time"
 
 	telegramBot "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/projectdiscovery/gologger"
 )
 
-func toTelegram(token string, chatId string, parseMode string, log map[string]string) {
-	id, err := strconv.Atoi(chatId)
+func toTelegram(token string, chatID string, parseMode string, log map[string]string) {
+	id, err := strconv.ParseInt(chatID, 10, 64)
 	if err != nil {
-		panic(err)
+		gologger.Errorf(err.Error())
 	}
 
 	api, err := telegramBot.NewBotAPI(token)
 	if err != nil {
-		panic(err)
+		gologger.Errorf(err.Error())
 	}
 
-	message := telegramBot.NewMessage(int64(id), telegramMessage(log))
+	message := telegramBot.NewMessage(id, telegramMessage(log))
 	message.ParseMode = parseMode
 
-	time.Sleep(1 * time.Second)
-	_, err = api.Send(message)
-	if err != nil {
-		time.Sleep(5 * time.Second)
+	for {
+		_, err = api.Send(message)
+		time.Sleep(1 * time.Second)
+		if err == nil {
+			break
+		}
 	}
 }
 
@@ -36,12 +39,12 @@ func telegramMessage(log map[string]string) string {
 
 	template, err := template.ParseFiles(fmt.Sprintf("internal/alert/template/telegram.tmpl"))
 	if err != nil {
-		panic(err)
+		gologger.Warningf(err.Error())
 	}
 
 	err = template.Execute(&buffer, log)
 	if err != nil {
-		panic(err)
+		gologger.Warningf(err.Error())
 	}
 
 	return buffer.String()
