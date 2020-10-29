9728e0bc0bae88a215bd75a9a2e1be929504ac6e
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-10-30 06:59:27 +0700

    :arrow_up: Bump to v0.0.1-dev.4.2

--- internal/runner/constants.go
+++ internal/runner/constants.go
@@ -2,7 +2,7 @@ package runner
 
 const (
 	email       = "infosec@kitabisa.com"
-	version     = "0.0.1-rc1.3"
+	version     = "0.0.1-dev.4.3"
 	development = true
 	banner      = `
 	  __      __       
