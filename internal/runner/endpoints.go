be16c6807b16be81d195825a1462e8f4f86ee25c
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-10-30 06:55:23 +0700

    :fire: Remove unused variable

--- internal/runner/endpoints.go
+++ internal/runner/endpoints.go
@@ -1,7 +1,5 @@
 package runner
 
 const (
-	SlackAPI    = "https://slack.com/api/chat.postMessage"
-	TelegramAPI = "https://api.telegram.org/bot:token/sendMessage"
-	Google204   = "http://clients3.google.com/generate_204"
+	Google204 = "http://clients3.google.com/generate_204"
 )
