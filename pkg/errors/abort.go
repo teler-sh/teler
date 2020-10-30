ef51a7ed6653a8a54f9080719dce2de56bb5980c
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-10-30 18:44:46 +0700

    :hammer: Add Abort for supporting cross-platform

---
+++ pkg/errors/abort.go
@@ -0,0 +1,15 @@
+// +build !windows
+
+package errors
+
+import "syscall"
+
+// Abort will terminate & sends SIGTERM to process
+func Abort(i ...int) {
+	pgid, err := syscall.Getpgid(syscall.Getpid())
+	if err != nil {
+		Exit(err.Error())
+	}
+
+	syscall.Kill(-pgid, syscall.SIGTERM)
+}
