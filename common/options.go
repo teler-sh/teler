aac90a39fba36312374cf901b9c243b56ec12ad5
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-10-31 18:22:15 +0700

    :rewind: Revert Prometheus metrics from cmd

--- common/options.go
+++ common/options.go
@@ -16,5 +16,4 @@ type Options struct {
 	Output      string           // Save detected threats to file
 	OutFile     *os.File         // Write log output into file
 	Configs     *parsers.Configs // Get teler configuration interface
-	Metrics     int              // Set port for metrics
 }
