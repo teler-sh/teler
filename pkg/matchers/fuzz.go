7b7141b0a6ed8bbfd06c02d6b5a5a1cd4f21f4c9
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-03-25 05:53:31 +0700

    :hammer: Mistype parameter for fuzzy find

--- pkg/matchers/fuzz.go
+++ pkg/matchers/fuzz.go
@@ -2,7 +2,7 @@ package matchers
 
 import "github.com/sahilm/fuzzy"
 
-func IsMatchFuzz(pattern string, s string) bool {
+func IsMatchFuzz(pattern string, s []string) bool {
 	matches := fuzzy.Find(pattern, s)
 	if len(matches) > 0 {
 		return true
